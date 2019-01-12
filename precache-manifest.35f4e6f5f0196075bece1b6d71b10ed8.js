0e50180168ce6ab25bf88f9cd44c50a5fb8cc345
Nikhil <nikhilvaidyar1997@gmail.com>
2019-01-12 16:20:37 +0530

    Updates

---
+++ precache-manifest.35f4e6f5f0196075bece1b6d71b10ed8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "466e874c6496e9da62dc",
+    "url": "/gif-generator/static/css/main.323b2031.chunk.css"
+  },
+  {
+    "revision": "466e874c6496e9da62dc",
+    "url": "/gif-generator/static/js/main.466e874c.chunk.js"
+  },
+  {
+    "revision": "5b265051865492349dc6",
+    "url": "/gif-generator/static/js/1.5b265051.chunk.js"
+  },
+  {
+    "revision": "5d7f5ba9756b7da44232",
+    "url": "/gif-generator/static/js/runtime~main.5d7f5ba9.js"
+  },
+  {
+    "revision": "9202f5d004d4633d34bdff086712054d",
+    "url": "/gif-generator/static/media/image.9202f5d0.gif"
+  },
+  {
+    "revision": "e0ffe14f6560e4f9cea4606464f76e66",
+    "url": "/gif-generator/index.html"
+  }
+];
